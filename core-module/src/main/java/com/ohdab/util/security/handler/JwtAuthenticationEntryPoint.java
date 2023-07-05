1c011b31d0f0cfa9069f6fc6b11518ec827b7024
simhani1 <simhani1@gmail.com>
2023-07-06 05:41:09 +0900

    style: spotless apply

--- core-module/src/main/java/com/ohdab/util/security/handler/JwtAuthenticationEntryPoint.java
+++ core-module/src/main/java/com/ohdab/util/security/handler/JwtAuthenticationEntryPoint.java
@@ -2,18 +2,17 @@ package com.ohdab.util.security.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ohdab.template.ErrorMessage;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 @Slf4j
 @Component
