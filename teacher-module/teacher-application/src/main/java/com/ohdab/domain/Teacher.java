3c4659278782456eb429590638f083d64e577836
linirini <2001yerin@naver.com>
2023-07-24 16:35:22 +0900

    feat : domain logic

--- teacher-module/teacher-application/src/main/java/com/ohdab/domain/Teacher.java
+++ teacher-module/teacher-application/src/main/java/com/ohdab/domain/Teacher.java
@@ -6,9 +6,30 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class Teacher {
 
-    private Long id;
+    private long id;
+
+    private String name;
     private List<Classroom> classrooms;
+
+    @Builder
+    public Teacher(String name) {
+        setName(name);
+    }
+
+    private void setName(String name) {
+        if (name.length() > 20) {
+            throw new IllegalStateException(
+                    "Name length cannot exceed 20 : current length \"" + name.length() + "\"");
+        }
+        this.name = name;
+    }
+
+    public void addClassroom(Classroom classroom) {
+        if (classroom == null) {
+            throw new IllegalStateException("Classroom cannot be null.");
+        }
+        this.classrooms.add(classroom);
+    }
 }
