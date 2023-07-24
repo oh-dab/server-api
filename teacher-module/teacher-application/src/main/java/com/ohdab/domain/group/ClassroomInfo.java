46ff6f181e522ba284736b3f009fccccd8939edf
linirini <101927543+linirini@users.noreply.github.com>
2023-07-24 16:51:32 +0900

    feat : domain logic (#16)

--- teacher-module/teacher-application/src/main/java/com/ohdab/domain/group/ClassroomInfo.java
+++ teacher-module/teacher-application/src/main/java/com/ohdab/domain/group/ClassroomInfo.java
@@ -4,9 +4,21 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class ClassroomInfo {
 
     private String name;
     private Grade grade;
+
+    @Builder
+    public ClassroomInfo(String name, Grade grade) {
+        setName(name);
+        this.grade = grade;
+    }
+
+    private void setName(String name) {
+        if (name.length() > 20) {
+            throw new IllegalStateException(
+                    "Name length cannot exceed 20 : current length \"" + name.length() + "\"");
+        }
+    }
 }
