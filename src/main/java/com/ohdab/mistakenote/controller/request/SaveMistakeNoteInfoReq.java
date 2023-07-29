fafdd7b3ed1422258bd31e11015b0c8b5c5baa88
simhani1 <simhani1@gmail.com>
2023-07-29 22:27:35 +0900

    fix: req 클래스 validation 추가

--- src/main/java/com/ohdab/mistakenote/controller/request/SaveMistakeNoteInfoReq.java
+++ src/main/java/com/ohdab/mistakenote/controller/request/SaveMistakeNoteInfoReq.java
@@ -1,5 +1,7 @@
 package com.ohdab.mistakenote.controller.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -8,5 +10,7 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SaveMistakeNoteInfoReq {
 
+    @NotNull(message = "기록할 문제 번호를 하나 이상 입력해야 합니다.")
+    @Size(min = 1, max = 6000)
     private int[] mistakeNumbers;
 }
