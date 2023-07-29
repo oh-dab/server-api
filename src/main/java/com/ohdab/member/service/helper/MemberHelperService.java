95c5515f1bbe20c5029cba5a88625929008842b5
Jonghan Sim <simhani1@gmail.com>
2023-07-30 00:28:30 +0900

    feat: 학생별 오답노트 조회 service 단위 테스트 & 구현 (#23)
    * test: 학생별 오답노트 조회 controller 단위 테스트
    
    * feat: 학생별 오답노트 조회 controller 구현
    
    * fix: 테스트 클래스 패키지 이동
    
    * style: spotless 적용
    
    * fix: DI 오류 수정
    
    * docs: rest docs 작성
    
    * test: service 단위 테스트
    
    * test: service 단위 테스트
    
    * style: 라인 공백
    
    * feat: 학생별 오답노트 조회 service 구현
    
    * test: 삭제 회원 검사로직 추가
    
    * test: 회원 삭제 여부 로직을 위한 테스트 추가
    
    * feat: 회원 삭제 여부 메서드 추가
    
    * fix: workbookRepository 주입 제거

--- src/main/java/com/ohdab/member/service/helper/MemberHelperService.java
+++ src/main/java/com/ohdab/member/service/helper/MemberHelperService.java
@@ -12,7 +12,7 @@ public final class MemberHelperService {
 
     private final MemberRepository memberRepository;
 
-    public Member findExistingMember(String name) {
+    public Member findExistingMemberByName(String name) {
         return memberRepository
                 .findByMemberInfoName(name)
                 .orElseThrow(() -> new NoMemberException("존재하지 않는 회원입니다."));
--- src/main/java/com/ohdab/mistakenote/service/GetMistakeNoteInfoService.java
+++ src/main/java/com/ohdab/mistakenote/service/GetMistakeNoteInfoService.java
@@ -1,8 +1,18 @@
 package com.ohdab.mistakenote.service;
 
+import com.ohdab.member.domain.student.studentid.StudentId;
+import com.ohdab.member.exception.NoMemberException;
+import com.ohdab.member.repository.MemberRepository;
+import com.ohdab.mistakenote.domain.MistakeNote;
+import com.ohdab.mistakenote.exception.NoWorkbookException;
+import com.ohdab.mistakenote.repository.MistakeNoteRepository;
 import com.ohdab.mistakenote.service.dto.MistakeNoteInfoDto;
+import com.ohdab.mistakenote.service.helper.MistakeNoteHelperService;
 import com.ohdab.mistakenote.service.usecase.GetMistakeNoteInfoUsecase;
+import com.ohdab.workbook.domain.workbookid.WorkbookId;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +20,34 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class GetMistakeNoteInfoService implements GetMistakeNoteInfoUsecase {
 
+    private final MistakeNoteHelperService mistakeNoteHelperService;
+    private final MistakeNoteRepository mistakeNoteRepository;
+    private final MemberRepository memberRepository;
+
     @Override
     public List<MistakeNoteInfoDto> getMistakeNoteInfoByStudent(long workbookId, long studentId) {
-        return null;
+        if (mistakeNoteHelperService.isNotExistingMember(memberRepository, studentId)) {
+            throw new NoMemberException("존재하지 않는 회원입니다.");
+        }
+        MistakeNote mistakeNote =
+                mistakeNoteRepository
+                        .findByWorkbookIdAndStudentId(
+                                new WorkbookId(workbookId), new StudentId(studentId))
+                        .orElseThrow(() -> new NoWorkbookException("존재하지 않는 오답노트입니다."));
+        return mapToMistakeNoteInfo(mistakeNote);
+    }
+
+    private List<MistakeNoteInfoDto> mapToMistakeNoteInfo(MistakeNote mistakeNote) {
+        List<MistakeNoteInfoDto> mistakeNoteInfo = new ArrayList<>();
+        Map<Integer, Integer> mistakeRecords = mistakeNote.getMistakeRecords();
+        mistakeRecords.forEach(
+                (number, count) -> {
+                    mistakeNoteInfo.add(
+                            MistakeNoteInfoDto.builder()
+                                    .wrongNumber(number)
+                                    .wrongCount(count)
+                                    .build());
+                });
+        return mistakeNoteInfo;
     }
 }
