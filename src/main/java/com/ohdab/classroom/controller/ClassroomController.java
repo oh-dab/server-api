c6e33ac3672f766a5d1f6544075e063a59fb03b1
seongha <11pi885@gmail.com>
2023-07-31 03:28:32 +0900

    feat : 반 추가 controller 작성 & 테스트

---
+++ src/main/java/com/ohdab/classroom/controller/ClassroomController.java
@@ -0,0 +1,31 @@
+package com.ohdab.classroom.controller;
+
+import com.ohdab.classroom.controller.mapper.ClassroomMapper;
+import com.ohdab.classroom.controller.request.ClassroomReq;
+import com.ohdab.classroom.controller.response.ClassroomRes;
+import com.ohdab.classroom.service.dto.ClassroomReqDto;
+import com.ohdab.classroom.service.usecase.ClassroomUsecase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/classrooms")
+public class ClassroomController {
+
+    private final ClassroomUsecase classroomUsecase;
+
+    @PostMapping("/enrollment")
+    public ResponseEntity<ClassroomRes> addClassroom(@Valid @RequestBody ClassroomReq classroomReq) {
+        ClassroomReqDto classroomReqDto =
+                ClassroomMapper.classroomReqToClassroomReqDto(classroomReq);
+        classroomUsecase.addClassroom(classroomReqDto);
+        return ResponseEntity.ok(ClassroomMapper.createClassRoomRes());
+    }
+}
---
+++ src/main/java/com/ohdab/classroom/controller/response/ClassroomRes.java
@@ -0,0 +1,11 @@
+package com.ohdab.classroom.controller.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ClassroomRes {
+    String message;
+
+}
