e95beb6ae3c96d3275a32b035d72c1644378f26a
seongha_h <11pi885@gmail.com>
2023-08-02 00:13:20 +0900

    feat: 반 추가 Repository 테스트 & 구현 (#27)
    * feat : addClassroom Service & test
    
    * style : spotless 적용
    
    * feat : repository 구현 & 테스트 작성
    
    * feat : repository 구현 & 테스트 작성
    
    * fix : 코드리뷰 변경사항 수정
    
    * style : spotless 적용
    
    * refactor : 인터페이스 이름 변경
    
    * Update src/main/java/com/ohdab/classroom/repository/ClassroomRepository.java
    
    Co-authored-by: Jonghan Sim <simhani1@gmail.com>
    
    ---------
    
    Co-authored-by: seongha <seongha@seongha-2.local>
    Co-authored-by: Jonghan Sim <simhani1@gmail.com>

---
+++ src/test/java/com/ohdab/classroom/repository/AddClassroomRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.ohdab.classroom.repository;
+
+import com.ohdab.classroom.domain.Classroom;
+import com.ohdab.classroom.domain.classroomInfo.ClassroomInfo;
+import com.ohdab.classroom.domain.classroomInfo.Grade;
+import com.ohdab.member.domain.teacher.teacherid.TeacherId;
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class AddClassroomRepositoryTest {
+
+    @Autowired private ClassroomRepository classroomRepository;
+    @Autowired private EntityManager em;
+
+    @Test
+    @DisplayName("반 추가 (저장) 테스트 -> 선생님 아이디를 통해 조회")
+    void 반_추가_테스트() {
+        // given
+        Long id = 1L;
+        TeacherId teacherId = TeacherId.builder().id(id).build();
+        String name = "1반";
+        String desc = "1반에 대한 설명입니다.";
+
+        ClassroomInfo classroomInfo =
+                ClassroomInfo.builder().name(name).description(desc).grade(Grade.HIGH_1).build();
+
+        Classroom classroom =
+                Classroom.builder().classroomInfo(classroomInfo).teacher(teacherId).build();
+
+        // when
+        classroomRepository.save(classroom);
+        List<Classroom> foundClassrooms = classroomRepository.findAllByTeacherId(teacherId.getId());
+        // then
+        Assertions.assertThat(foundClassrooms.get(0).getTeacher().getId())
+                .isEqualTo(teacherId.getId());
+        Assertions.assertThat(foundClassrooms.get(0).getClassroomInfo().getName()).isEqualTo(name);
+        Assertions.assertThat(foundClassrooms.get(0).getClassroomInfo().getDescription())
+                .isEqualTo(desc);
+        Assertions.assertThat(foundClassrooms.get(0).getClassroomInfo().getGrade())
+                .isEqualTo(Grade.HIGH_1);
+    }
+}
