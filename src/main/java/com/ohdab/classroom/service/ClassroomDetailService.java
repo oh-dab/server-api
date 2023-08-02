be4e4e1220d205115d4066142665769a740c1bca
seongha <11pi885@gmail.com>
2023-08-03 01:17:51 +0900

    feat : 반 상세조회 Service 구현

---
+++ src/main/java/com/ohdab/classroom/service/ClassroomDetailService.java
@@ -0,0 +1,25 @@
+package com.ohdab.classroom.service;
+
+import com.ohdab.classroom.domain.Classroom;
+import com.ohdab.classroom.repository.ClassroomRepository;
+import com.ohdab.classroom.service.mapper.ClassroomDetailServiceMapper;
+import com.ohdab.classroom.service.usecase.ClassroomDetailUsecase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.ohdab.classroom.service.dto.ClassroomDetailDto.ClassroomDetailDtoResponse;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ClassroomDetailService implements ClassroomDetailUsecase {
+
+    private final ClassroomRepository classroomRepository;
+
+    @Override
+    public ClassroomDetailDtoResponse getClassroomDetailById(long classroomId) {
+        Classroom classroom = classroomRepository.findClassroomById(classroomId);
+        return ClassroomDetailServiceMapper.classroomToClassroomDetail(classroom);
+    }
+}
