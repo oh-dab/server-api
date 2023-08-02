b5b46adf323aa7f431565e37365882da9cdf754b
simhani1 <simhani1@gmail.com>
2023-08-03 03:22:48 +0900

    fix: mapper 클래스 경로 수정

--- src/main/java/com/ohdab/member/repository/mapper/MemberMapper.java
+++ src/main/java/com/ohdab/member/repository/mapper/MemberMapper.java
@@ -1,10 +1,9 @@
-package com.ohdab.member.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-import java.util.List;
+package com.ohdab.member.repository.mapper;
 
 import static com.ohdab.mistakenote.service.dto.GetAllMistakeNoteInfoDto.Response.StudentInfoDto;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
