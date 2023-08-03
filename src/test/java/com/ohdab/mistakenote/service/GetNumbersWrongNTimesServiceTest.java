16772391cc5239022aa6ab8485aa9ca9c073c641
simhani1 <simhani1@gmail.com>
2023-08-04 02:29:33 +0900

    fix: 문제 범위에 대한 test 추가

--- src/test/java/com/ohdab/mistakenote/service/GetNumbersWrongNTimesServiceTest.java
+++ src/test/java/com/ohdab/mistakenote/service/GetNumbersWrongNTimesServiceTest.java
@@ -2,14 +2,21 @@ package com.ohdab.mistakenote.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchException;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
+import com.ohdab.classroom.domain.classroomid.ClassroomId;
 import com.ohdab.mistakenote.exception.NoNumbersWrongNTimesException;
+import com.ohdab.mistakenote.exception.NumberIsOutOfRangeException;
 import com.ohdab.mistakenote.repository.mapper.MistakeRecordMapper;
 import com.ohdab.mistakenote.service.dto.GetNumbersWrongNTimesDto;
 import com.ohdab.mistakenote.service.usecase.GetNumbersWrongNTimesUsecase;
+import com.ohdab.workbook.domain.Workbook;
+import com.ohdab.workbook.domain.workbookInfo.WorkbookInfo;
+import com.ohdab.workbook.repository.WorkbookRepository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +31,7 @@ class GetNumbersWrongNTimesServiceTest {
     @Autowired private GetNumbersWrongNTimesUsecase getNumbersWrongNTimesUsecase;
 
     @MockBean private MistakeRecordMapper mistakeRecordMapper;
+    @MockBean private WorkbookRepository workbookRepository;
 
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
     @Nested
@@ -31,52 +39,131 @@ class GetNumbersWrongNTimesServiceTest {
 
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
         @Nested
-        class 요청받은_범위_내에서_N번_이상_틀린_문제가_존재한다면 {
+        class 요청받은_범위가_해당_교재의_문제범위에_속하고 {
 
-            @DisplayName("틀린 문제번호들을 쉼표로 구분하여 공백없는 문자열로 반환한다")
-            @Test
-            void 틀린_문제번호들을_쉼표로_구분하여_공백없는_문자열로_반환한다() {
-                // given
-                final GetNumbersWrongNTimesDto.Request requestDto =
-                        GetNumbersWrongNTimesDto.Request.builder()
-                                .mistakeNoteId(1L)
-                                .count(2)
-                                .from(10)
-                                .to(20)
-                                .build();
-                final List<Integer> numbersWrongNTimes = List.of(10, 11, 12, 19, 20);
+            @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+            @Nested
+            class N번_이상_틀린_문제가_존재한다면 {
+                @DisplayName("틀린 문제번호들을 쉼표로 구분하여 공백없는 문자열로 반환한다")
+                @Test
+                void 틀린_문제번호들을_쉼표로_구분하여_공백없는_문자열로_반환한다() {
+                    // given
+                    final GetNumbersWrongNTimesDto.Request requestDto =
+                            GetNumbersWrongNTimesDto.Request.builder()
+                                    .workbookId(1L)
+                                    .mistakeNoteId(2L)
+                                    .count(2)
+                                    .from(10)
+                                    .to(20)
+                                    .build();
 
-                // when
-                when(mistakeRecordMapper.findNumbersWrongNTimes(requestDto))
-                        .thenReturn(numbersWrongNTimes);
-                GetNumbersWrongNTimesDto.Response result =
-                        getNumbersWrongNTimesUsecase.getNumbersWrongNTimes(requestDto);
+                    final Workbook workbook =
+                            Workbook.builder()
+                                    .classroomId(new ClassroomId(10L))
+                                    .workbookInfo(
+                                            WorkbookInfo.builder()
+                                                    .name("책이름")
+                                                    .description("설명")
+                                                    .startingNumber(1)
+                                                    .endingNumber(1000)
+                                                    .build())
+                                    .build();
 
-                // then
-                assertThat(result).extracting("wrongNumbers").isEqualTo("10,11,12,19,20");
+                    final List<Integer> numbersWrongNTimes = List.of(10, 11, 12, 19, 20);
+
+                    // when
+                    when(workbookRepository.findById(anyLong()))
+                            .thenReturn(Optional.ofNullable(workbook));
+                    when(mistakeRecordMapper.findNumbersWrongNTimes(requestDto))
+                            .thenReturn(numbersWrongNTimes);
+                    GetNumbersWrongNTimesDto.Response result =
+                            getNumbersWrongNTimesUsecase.getNumbersWrongNTimes(requestDto);
+
+                    // then
+                    assertThat(result).extracting("wrongNumbers").isEqualTo("10,11,12,19,20");
+                }
+            }
+
+            @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+            @Nested
+            class N번_이상_틀린_문제가_없다면 {
+
+                @DisplayName("NoNumbersWrongNTimesException 예외를 던진다.")
+                @Test
+                void NoNumbersWrongNTimesException_예외를_발생시킨다() {
+                    // given
+                    final GetNumbersWrongNTimesDto.Request requestDto =
+                            GetNumbersWrongNTimesDto.Request.builder()
+                                    .workbookId(1L)
+                                    .mistakeNoteId(2L)
+                                    .count(2)
+                                    .from(10)
+                                    .to(20)
+                                    .build();
+
+                    final Workbook workbook =
+                            Workbook.builder()
+                                    .classroomId(new ClassroomId(10L))
+                                    .workbookInfo(
+                                            WorkbookInfo.builder()
+                                                    .name("책이름")
+                                                    .description("설명")
+                                                    .startingNumber(1)
+                                                    .endingNumber(1000)
+                                                    .build())
+                                    .build();
+
+                    final List<Integer> numbersWrongNTimes = new ArrayList<>();
+
+                    // when
+                    when(workbookRepository.findById(anyLong()))
+                            .thenReturn(Optional.ofNullable(workbook));
+                    when(mistakeRecordMapper.findNumbersWrongNTimes(requestDto))
+                            .thenReturn(numbersWrongNTimes);
+                    Throwable thrown =
+                            catchException(
+                                    () ->
+                                            getNumbersWrongNTimesUsecase.getNumbersWrongNTimes(
+                                                    requestDto));
+
+                    // then
+                    assertThat(thrown).isInstanceOf(NoNumbersWrongNTimesException.class);
+                }
             }
         }
 
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
         @Nested
-        class 요청받은_범위_내에서_N번_이상_틀린_문제가_없다면 {
+        class 요청받은_범위가_해당_교재의_문제범위에_속하지_않는다면 {
 
-            @DisplayName("NoNumbersWrongNTimesException 예외를 발생시킨다.")
+            @DisplayName("NumberIsOutOfRangeException 예외를 던진다.")
             @Test
-            void NoNumbersWrongNTimesException_예외를_발생시킨다() {
+            void NumberIsOutOfRangeException_예외를_던진다() {
                 // given
                 final GetNumbersWrongNTimesDto.Request requestDto =
                         GetNumbersWrongNTimesDto.Request.builder()
-                                .mistakeNoteId(1L)
+                                .workbookId(1L)
+                                .mistakeNoteId(2L)
                                 .count(2)
-                                .from(10)
-                                .to(20)
+                                .from(20)
+                                .to(2000)
                                 .build();
-                final List<Integer> numbersWrongNTimes = new ArrayList<>();
+
+                final Workbook workbook =
+                        Workbook.builder()
+                                .classroomId(new ClassroomId(10L))
+                                .workbookInfo(
+                                        WorkbookInfo.builder()
+                                                .name("책이름")
+                                                .description("설명")
+                                                .startingNumber(1)
+                                                .endingNumber(1000)
+                                                .build())
+                                .build();
 
                 // when
-                when(mistakeRecordMapper.findNumbersWrongNTimes(requestDto))
-                        .thenThrow(NoNumbersWrongNTimesException.class);
+                when(workbookRepository.findById(anyLong()))
+                        .thenReturn(Optional.ofNullable(workbook));
                 Throwable thrown =
                         catchException(
                                 () ->
@@ -84,7 +171,7 @@ class GetNumbersWrongNTimesServiceTest {
                                                 requestDto));
 
                 // then
-                assertThat(thrown).isInstanceOf(NoNumbersWrongNTimesException.class);
+                assertThat(thrown).isInstanceOf(NumberIsOutOfRangeException.class);
             }
         }
     }
