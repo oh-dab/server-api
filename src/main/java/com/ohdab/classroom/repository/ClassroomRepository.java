5c3aa6cd08cee92d93d0c34832580602fcff9a26
seongha_h <11pi885@gmail.com>
2023-08-08 16:10:33 +0900

    feat/#55 반 상세조회 Service 작성 (#69)
    * feat : addClassroom Service & test
    
    * style : spotless 적용
    
    * feat : repository 구현 & 테스트 작성
    
    * feat : repository 구현 & 테스트 작성
    
    * feat : 반 추가 controller 작성 & 테스트
    
    * style : spotless 적용
    
    * fix : 코드리뷰 변경사항 수정
    
    * style : spotless 적용
    
    * docs : 반 추가 api 문서 추가
    
    * feat : findClassroomService 구현 및 ClassroomDto 리팩토링
    
    * refactor : 인터페이스 이름 변경
    
    * style : spotyless 적용
    
    * fix : addClassroom 인터페이스 이름 변경
    
    * style : spotless 적용
    
    * test : 반 목록 조회 테스트 작성
    
    * feat : 반 목록 조회 controller 구현 & test 작성
    
    * fix : Controller dto, request, response 객체 수정
    
    * feat : 반 상세조회 Service 구현
    
    * fix : controller dto 변경, getClassroomListByTeacherId 인자를 query String 으로 변경
    
    * style : spotless 적용
    
    * style : optimize import
    
    * style : optimize import
    
    * style : spotless 적용
    
    ---------
    
    Co-authored-by: seongha <seongha@seongha-2.local>

--- src/main/java/com/ohdab/classroom/repository/ClassroomRepository.java
+++ src/main/java/com/ohdab/classroom/repository/ClassroomRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
 
     List<Classroom> findAllByTeacherId(long teacherId);
+
+    Classroom findClassroomById(long classroomId);
 }
