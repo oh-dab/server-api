32ac8a5c4d64033d8f919608d3791a3d83775e2c
seongha_h <11pi885@gmail.com>
2023-08-08 16:10:56 +0900

    Feat/#56 반 상세조회 Repository Test 작성 (#70)
    * feat : addClassroom Service & test
    
    * style : spotless 적용
    
    * feat : repository 구현 & 테스트 작성
    
    * feat : repository 구현 & 테스트 작성
    
    * feat : 반 추가 controller 작성 & 테스트
    
    * style : spotless 적용
    
    * fix : 코드리뷰 변경사항 수정
    
    * style : spotless 적용
    
    * docs : 반 추가 api 문서 추가
    
    * feat : findClassroomService 구현 및 ClassroomDto 리팩토링
    
    * refactor : 인터페이스 이름 변경
    
    * style : spotyless 적용
    
    * fix : addClassroom 인터페이스 이름 변경
    
    * style : spotless 적용
    
    * test : 반 목록 조회 테스트 작성
    
    * feat : 반 목록 조회 controller 구현 & test 작성
    
    * fix : Controller dto, request, response 객체 수정
    
    * feat : 반 상세조회 Service 구현
    
    * fix : controller dto 변경, getClassroomListByTeacherId 인자를 query String 으로 변경
    
    * style : spotless 적용
    
    * style : optimize import
    
    * style : optimize import
    
    * style : spotless 적용
    
    * test : 반 상세조회 repository test 작성
    
    * fix : test 버그 수정 -> 전체 테스트시 Id 값이 변경되어 테스트 실패하는 것 수정
    
    ---------
    
    Co-authored-by: seongha <seongha@seongha-2.local>

---
+++ src/test/java/com/ohdab/classroom/repository/FindClassroomByIdRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.ohdab.classroom.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.ohdab.classroom.domain.Classroom;
+import com.ohdab.classroom.domain.classroomInfo.ClassroomInfo;
+import com.ohdab.classroom.domain.classroomInfo.Grade;
+import com.ohdab.member.domain.student.studentid.StudentId;
+import com.ohdab.member.domain.teacher.teacherid.TeacherId;
+import com.ohdab.workbook.domain.workbookid.WorkbookId;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class FindClassroomByIdRepositoryTest {
+
+    @Autowired private ClassroomRepository classroomRepository;
+    @Autowired private EntityManager em;
+
+    @Test
+    void 반_상세조회() {
+
+        // given
+        Classroom classroom1 =
+                Classroom.builder()
+                        .teacher(TeacherId.builder().id(2L).build())
+                        .classroomInfo(
+                                ClassroomInfo.builder()
+                                        .name("1반")
+                                        .description("1반 설명")
+                                        .grade(Grade.HIGH_1)
+                                        .build())
+                        .build();
+        classroom1.addStudent(StudentId.builder().id(3).build());
+        classroom1.addWorkbook(WorkbookId.builder().id(4).build());
+        classroomRepository.save(classroom1);
+        long id = classroom1.getId();
+        // when
+        Classroom foundClassroom = classroomRepository.findClassroomById(id);
+
+        // then
+        assertThat(foundClassroom.getId()).isEqualTo(id);
+        assertThat(foundClassroom.getTeacher().getId()).isEqualTo(2);
+        assertThat(foundClassroom.getClassroomInfo().getName()).isEqualTo("1반");
+        assertThat(foundClassroom.getClassroomInfo().getDescription()).isEqualTo("1반 설명");
+        assertThat(foundClassroom.getClassroomInfo().getGrade()).isEqualTo(Grade.HIGH_1);
+        assertThat(foundClassroom.getStudents().get(0).getId()).isEqualTo(3);
+        assertThat(foundClassroom.getWorkbooks().get(0).getId()).isEqualTo(4);
+    }
+}
