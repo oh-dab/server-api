f2f41939fb26f8955924bb6e25ce15d25bc38f29
linirini <2001yerin@naver.com>
2023-08-13 02:28:26 +0900

    fix : saveMistakeNote 수정

--- src/main/java/com/ohdab/classroom/service/AddWorkbookService.java
+++ src/main/java/com/ohdab/classroom/service/AddWorkbookService.java
@@ -15,7 +15,6 @@ import com.ohdab.workbook.domain.Workbook;
 import com.ohdab.workbook.domain.workbookInfo.WorkbookInfo;
 import com.ohdab.workbook.domain.workbookid.WorkbookId;
 import com.ohdab.workbook.repository.WorkbookRepository;
-import java.util.HashMap;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -77,7 +76,6 @@ public class AddWorkbookService implements AddWorkbookUsecase {
                             MistakeNote.builder()
                                     .studentId(studentId)
                                     .workbookId(workbookId)
-                                    .mistakeRecords(new HashMap<>())
                                     .build();
                     mistakeNoteRepository.save(mistakeNote);
                 });
