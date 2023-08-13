8d7c11618ada207b8b64d588523170315c44ce39
simhani1 <simhani1@gmail.com>
2023-08-14 00:59:31 +0900

    feat: transactional 추가

--- src/main/java/com/ohdab/classroom/service/AddStudentService.java
+++ src/main/java/com/ohdab/classroom/service/AddStudentService.java
@@ -13,9 +13,11 @@ import com.ohdab.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AddStudentService implements AddStudentUsecase {
 
     private final ApplicationEventPublisher publisher;
