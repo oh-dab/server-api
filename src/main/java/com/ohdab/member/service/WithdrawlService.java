e70dd1bfef1ec86d3ca8617282e9707710ff643f
Jonghan Sim <simhani1@gmail.com>
2023-08-16 04:09:11 +0900

    feat: 회원탈퇴 service 단위 테스트 & 구현 (#146)
    * test: service 단위 테스트
    
    * feat: service 구현

--- src/main/java/com/ohdab/member/service/WithdrawlService.java
+++ src/main/java/com/ohdab/member/service/WithdrawlService.java
@@ -1,5 +1,9 @@
 package com.ohdab.member.service;
 
+import static com.ohdab.member.service.helper.MemberHelperService.findExistingMemberById;
+
+import com.ohdab.member.domain.Member;
+import com.ohdab.member.repository.MemberRepository;
 import com.ohdab.member.service.usecase.WithdrawlUsecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +14,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class WithdrawlService implements WithdrawlUsecase {
 
+    private final MemberRepository memberRepository;
+
     @Override
-    public void withdrawl(long memberId) {}
+    public void withdrawl(long memberId) {
+        Member member = findExistingMemberById(memberRepository, memberId);
+        member.withdrawal();
+    }
 }
