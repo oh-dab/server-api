295029a681fa2c22facd4ffa80eac53548098706
Jonghan Sim <simhani1@gmail.com>
2023-08-15 15:11:24 +0900

    feat: 학생 추가(회원 가입 요청, 오답노트 생성) service 단위 테스트 & 구현 (#132)
    * test: controller 단위 테스트
    
    * feat: controller 구현
    
    * feat: servie 구현 및 테스트, 이벤트 추가
    
    * test: studentAddedHandler 단위 테스트
    
    * fix: 테스트 패키지 경로 복구
    
    * feat: transactional 추가
    
    * docs: rest docs 추가
    
    * test: MockBean 추가
    
    * feat: 학생 추가시 오답노트 생성 로직 추가

--- src/test/java/com/ohdab/classroom/service/AddWorkbookServiceTest.java
+++ src/test/java/com/ohdab/classroom/service/AddWorkbookServiceTest.java
@@ -2,9 +2,7 @@ package com.ohdab.classroom.service;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import com.ohdab.classroom.domain.Classroom;
 import com.ohdab.classroom.domain.classroomInfo.ClassroomInfo;
