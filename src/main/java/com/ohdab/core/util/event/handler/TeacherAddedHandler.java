f5d1183b4787f4102ea0daf89aa7d7fdc4d9315a
linirini <2001yerin@naver.com>
2023-08-16 12:22:40 +0900

    feat : 선생님 추가 시 회원가입 성공 이벤트 테스트 작성

--- src/main/java/com/ohdab/core/util/event/handler/TeacherAddedHandler.java
+++ src/main/java/com/ohdab/core/util/event/handler/TeacherAddedHandler.java
@@ -1,6 +1,5 @@
 package com.ohdab.core.util.event.handler;
 
-import com.ohdab.classroom.event.StudentAddedEvent;
 import com.ohdab.member.event.TeacherAddedEvent;
 import com.ohdab.member.service.dto.MemberDtoForJoin;
 import com.ohdab.member.service.usecase.JoinUsecase;
@@ -16,10 +15,10 @@ public class TeacherAddedHandler {
     private final JoinUsecase joinUsecase;
 
     @EventListener(TeacherAddedEvent.class)
-    public void join(StudentAddedEvent event) {
+    public void join(TeacherAddedEvent event) {
         joinUsecase.join(
                 MemberDtoForJoin.Request.builder()
-                        .name(event.getStudentName())
+                        .name(event.getName())
                         .password(event.getPassword())
                         .role(List.of("TEACHER"))
                         .build());
