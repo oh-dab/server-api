6a2d515bc61f7e96dae3d5944d5334c9d4bce7e2
linirini <2001yerin@naver.com>
2023-08-18 23:38:07 +0900

    fix : 예외처리 삭제

--- src/main/java/com/ohdab/core/util/event/handler/TeacherDeletedHandler.java
+++ src/main/java/com/ohdab/core/util/event/handler/TeacherDeletedHandler.java
@@ -1,7 +1,6 @@
 package com.ohdab.core.util.event.handler;
 
 import com.ohdab.member.domain.Member;
-import com.ohdab.member.domain.MemberStatus;
 import com.ohdab.member.event.TeacherDeletedEvent;
 import com.ohdab.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,15 +16,7 @@ public class TeacherDeletedHandler {
     @EventListener(TeacherDeletedEvent.class)
     public void withdrawal(TeacherDeletedEvent event) {
         Member member = event.getMember();
-        throwIfInactiveMember(member);
         member.withdrawal();
         memberRepository.save(member);
-    }
-
-    private void throwIfInactiveMember(Member member) {
-        if (member.getStatus() == MemberStatus.INACTIVE) {
-            throw new IllegalStateException(
-                    "Already withdrawal Member with id \"" + member.getId() + "\"");
-        }
     }
 }
