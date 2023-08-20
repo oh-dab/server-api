8d58662f82a84107a38911904ec6f6ac41a41859
simhani1 <simhani1@gmail.com>
2023-08-21 00:20:48 +0900

    fix: 클래스 삭제

--- src/main/java/com/ohdab/classroom/service/usecase/ClassroomDetailUsecase.java
+++
@@ -1,8 +0,0 @@
-package com.ohdab.classroom.service.usecase;
-
-import static com.ohdab.classroom.service.dto.ClassroomDetailDto.ClassroomDetailDtoResponse;
-
-public interface ClassroomDetailUsecase {
-
-    ClassroomDetailDtoResponse getClassroomDetailById(long classroomId);
-}
