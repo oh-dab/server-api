0cf0c1a780be291882a5c59b45c1e3ea74e3bb94
simhani1 <simhani1@gmail.com>
2023-08-21 00:22:47 +0900

    fix: 테스트 클래스 접근 제어자 수정

--- src/test/java/com/ohdab/classroom/service/UpdateWorkbookInfoDtoServiceTest.java
+++ src/test/java/com/ohdab/classroom/service/UpdateWorkbookInfoDtoServiceTest.java
@@ -21,7 +21,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {UpdateWorkbookInfoService.class})
-public class UpdateWorkbookInfoDtoServiceTest {
+class UpdateWorkbookInfoDtoServiceTest {
 
     @Autowired private UpdateWorkbookInfoUsecase updateWorkbookInfoUsecase;
     @MockBean private WorkbookRepository workbookRepository;
